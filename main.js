6dcf036b3b1577f1e4c66971c6122712c0f0f68d
ngammil <ngammil@wgu.edu>
2024-10-17 11:51:53 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+// wait for the html elements to be selectable
+window.onload = setup
+
+function setup () {
+
+    const div = d3.select('#main-vis')
+
+    const tinyData = [
+        {w: '100', h: '100', x: '100', y: '100', c: 'blue'},
+        {w: '90', h: '90', x: '110', y: '110', c: 'lemonchiffon'},
+        {w: '70', h: '70', x: '130', y: '130', c: 'red'}
+    ]
+
+    const svg = d3.create('svg')
+        .attr('width', 400)
+        .attr('height', 400)
+
+    svg.selectAll('circle')
+        .data(tinyData)
+        .join('circle')
+        .attr("cx", row => row.x)
+        .attr("cy", row => row.y)
+        .attr("r", row => row.h)
+        .attr("fill", row => row.c);
+    
+    div.append(svg)
+}
+
